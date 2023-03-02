8d12c83c2e43e487935f9827a66ebb5736c03445
ryfazrin <riansera515@gmail.com>
2023-03-02 22:40:02 +0700

    added: dynamic form TOTAL product

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,52 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { ref } from 'vue';
 
+// data
 const products = ref([
   {
     name: "",
     price: 0,
-    qty: 0,
+    qty: 1,
     total: 0
   },
 ]);
 
+// Add form input
 function addMore() {
   products.value.push({
     name: "",
     price: 0,
-    qty: 0,
+    qty: 1,
     total: 0
   });
 }
 
+// Remove 
 function remove(index) {
   products.value.splice(index, 1);
 }
+
+// update TOTAL product
+// func handle Price
+function updatePrice(event, index) {
+  let product = products.value[index]
+  product.total = event.target.value * product.qty
+}
+
+// func handle Quantity
+function updateQuantity(event, index) {
+  let product = products.value[index]
+  
+  if (event.target.value < 1 & event.target.value != '') {
+    alert('Quantity tidak boleh kurang dari 1.')
+
+    product.qty = 1
+    event.target.value = 1
+  }
+
+  product.total = event.target.value * product.price
+}
+
 </script>
 
 <template>
@@ -31,15 +56,15 @@ function remove(index) {
     </button>
     <div>
       <div v-for="(product, index) in products" :key="index">
-        <label>Product name {{ product.name }}</label>
+        <label>Product name</label>
         <input type="text" v-model="product.name" />
         <label>Product price</label>
-        <input type="number" v-model="product.price" />
+        <input type="number" @input="updatePrice($event, index)" v-model="product.price" />
         <label>Qty</label>
-        <input type="number" v-model="product.qty" />
+        <input type="number" min="1" v-model="product.qty" @input="updateQuantity($event, index)" />
         <label>Total</label>
         <input type="number" v-model="product.total" disabled />
-        <button type="button" @click="remove(index)" v-show="index != 0">
+        <button type="button" v-show="index != 0" @click="remove(index)">
           Delete
         </button>
       </div>
